980699df026caa716c1305555011b35a00ced31f
Maxwell Castle <maxwell.j.castle@gmail.com>
2025-11-03 02:53:08 +0000

    JandT code done

--- JohnsonTrotter.cpp
+++ JohnsonTrotter.cpp
@@ -12,11 +12,20 @@ struct mobileNum{
 	int dir;		// -1 for left 1 for right
 };
 
+void swapItems(mobileNum& m, mobileNum& n){
+
+	mobileNum temp = m;
+	m = n;
+	n = temp;
+	//simple swap function through pass by ref
+
+}
+
 //function stolen from my paper1
-void printvec(vector<int> m){
+void printvec(vector<mobileNum> m){
 	int len = m.size();
 	for(int i = 0; i<len; i++){
-		printf("%d ", m[i]);
+		printf("%d ", m[i].value);
 	}
 	printf("\n");
 }
@@ -25,13 +34,46 @@ void printvec(vector<int> m){
 void johnsonTrotter(vector<mobileNum>& curr){
 
 	//
-	//START OF MIN-CHANGE CODE
-	//
+	//START OF PERMUTATION CODE
+	//	
+
+	while(true){
+		int mobileVal = -1;
+	    	int mobileIdx = -1;
+	    	int n = curr.size();
+		int primVal;
+		int primIdx;
 	
+	    	for (int i = 0; i < n; i++) {			//loops through all variables in curr
+	    	    int neighborIdx = i + curr[i].dir;		//tells us the neighbor's
+	
+	    	    if (neighborIdx >= 0 && neighborIdx < n) {
+	    	        if (curr[i].value > curr[neighborIdx].value && curr[i].value > mobileVal) {
+	    	            mobileVal = curr[i].value;
+	    	            mobileIdx = i;
+	    	        }
+	    	    }
+	    	}						//this will return us the Idx of the greatest mobile
+								//element
+		if (mobileIdx == -1){break;}			//if there is none, break. all done.
+		int neighborIdx2 = mobileIdx + curr[mobileIdx].dir;
+		swapItems(curr[mobileIdx], curr[neighborIdx2]); 	//swap the swappable neighbors
+		
+		primIdx = neighborIdx2;				//reassign stuff after swap for one more operation
+		primVal = curr[primIdx].value;
+
+		// loops through all numbers and flips the direction of the numbers greater than primVal
+		for (int i = 0; i<n; i++){
+			if (primVal < curr[i].value){
+				curr[i].dir *= -1;
+			}
+		}
+		printvec(curr);
+	}
 
 
 	//
-	//END OF MIN-CHANGE CODE
+	//END OF PERMUTATION CODE
 	//
 }
 
