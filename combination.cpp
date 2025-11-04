d26f62831d376d66e6bc2bfca1e5f5bc698bdee2
Maxwell Castle <maxwell.j.castle@gmail.com>
2025-11-04 23:45:46 +0000

    done done

--- combination.cpp
+++ combination.cpp
@@ -17,40 +17,38 @@ void printvec(vector<int> m){
 }
 
 void createCombo(int start, int n, int r, vector<int>& curr){
-	// Base case: combination is complete
-    	if ((int)curr.size() == r) {
+    	if ((int)curr.size() == r) {				//base case. comboniation is done
         	printvec(curr);
         	return;
     	}
 
-    	// Try adding each next element
     	for (int i = start; i <= n; i++) {
-        	curr.push_back(i);                 // include i
-        	createCombo(i + 1, n, r, curr);  // recurse
-        	curr.pop_back();                   // backtrack
+        	curr.push_back(i);                 		// include i
+        	createCombo(i + 1, n, r, curr);  		// recurse
+        	curr.pop_back();                   		// backtrack
     	}
 }
 
 void combination(int n, int r){
-	vector<int> result;
-	createCombo(1, n, r, result);
+	vector<int> result;						//intermediate function for initial recursion
+	createCombo(1, n, r, result);					//abstraction helped me understand parameters 
 }
 
 int main (){
 
-	int n;	 							//init n
-	printf("What is the number of permutations you'd like to try? (n): ");
+	int n;	 								//init n permutations num
+	printf("What is the number of permutations you'd like to try? (n): ");	
 	cin >> n;
 	if (n < 0){printf("\nNumber must be greater than 0\n");}
 
-	int r;
+	int r;									//choose number
 	printf("choose? (r): ");
 	cin >> r;
 	if (n < 0){printf("\nNumber must be greater than 0\n");}
 
 	auto start = high_resolution_clock::now(); 			// start a timer for our algorithm(s)
 
-	combination(n, r);
+	combination(n, r);						//call our created function
 
 	auto end = high_resolution_clock::now();
 	auto duration = duration_cast<milliseconds>(end - start); 	//end the clock and find the time taken in ms
